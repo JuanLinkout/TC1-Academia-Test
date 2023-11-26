3c43d7e688088413479a311967f4ff9cae8c072f
Guilherme <guilhermelopes035@gmail.com>
2023-11-26 15:48:10 -0300

    fix: add throw SeleniunTest

--- src/test/java/example/SeleniumTest.java
+++ src/test/java/example/SeleniumTest.java
@@ -2,12 +2,14 @@ package example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
     @Test
     @DisplayName("Test")
-    public void test(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver");
+    public void test() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         Thread.sleep(1000);
